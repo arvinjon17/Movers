a620d30d98729092528436fd698e6cb4e4efaf07
Ian Conrad B. Baarde <108701921+devianconrad@users.noreply.github.com>
2024-10-31 13:53:50 +0800

    fix config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8080/Movers/';
+// $config['base_url'] = 'http://localhost:8080/Movers/';
+$config['base_url'] = 'core2.moverstaxi.com';
+
 
 /*
 |--------------------------------------------------------------------------
